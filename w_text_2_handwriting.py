fab0ec2bed22ad7bb9ffe294a71f904ab8129f34
real-4w <real_4w@protonmail.com>
2021-10-27 17:39:15 +1300

    WIP

--- w_text_2_handwriting.py
+++ w_text_2_handwriting.py
@@ -5,17 +5,16 @@ sheet_width=BG.width
 gap, ht = 0, 0
 
 for i in txt.read().replace("\n",""):
-        cases = Image.open(f"myfont/{str(ord(i))}.png")
-        BG.paste(cases, (gap, ht))
-        size = cases.width
-        height=cases.height
-        #print(size)
-        gap+=size
+    cases = Image.open(f"myfont/{ord(i)}.png")
+    BG.paste(cases, (gap, ht))
+    size = cases.width
+    height=cases.height
+    #print(size)
+    gap+=size
+    if sheet_width < gap or len(i)*115 >(sheet_width-gap):
+        gap,ht=0,ht+140
 
-        if sheet_width < gap or len(i)*115 >(sheet_width-gap):
-            gap,ht=0,ht+140
-
-print(gap)
-print(sheet_width)
+#print(gap)
+#print(sheet_width)
 BG.show()
 
